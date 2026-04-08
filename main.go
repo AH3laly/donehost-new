8c15626b967d847a47309016d136ab2353d44a08
Ah3laly <Ah3laly@gmail.com>
2026-04-08 12:22:07 +0200

    Update message 44

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello from Donehost server 33")
+	fmt.Fprintln(w, "Hello from Donehost server 44")
 }
 
 func main() {
