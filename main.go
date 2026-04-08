41b9a8fafe82c2eb836682f25eadf3a249c1f634
Ah3laly <Ah3laly@gmail.com>
2026-04-08 11:29:17 +0200

    Update

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello from Donehost server")
+	fmt.Fprintln(w, "Hello from Donehost server 11")
 }
 
 func main() {
